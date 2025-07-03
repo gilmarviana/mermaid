b62cf65d78ac9150d3872cbc9479111dc4938669
Gilmar Viana <70184391+gilmarviana@users.noreply.github.com>
2025-07-03 02:18:58 -0300

    Corrigir PWA: usar ícones locais e remover manifest duplicado

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
       manifest: {
         name: 'Portfolio Gilmar Martinez Viana',
         short_name: 'Gilmar Martinez',
@@ -20,15 +23,15 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: 'https://c6f442293fa73eb40b2178fa58fd3742.cdn.bubble.io/cdn-cgi/image/w=192,h=192,f=auto,dpr=1,fit=contain/f1720976231275x392427182868092300/logo.jpg',
+            src: '/icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/jpeg',
+            type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'https://c6f442293fa73eb40b2178fa58fd3742.cdn.bubble.io/cdn-cgi/image/w=512,h=512,f=auto,dpr=1,fit=contain/f1720976231275x392427182868092300/logo.jpg',
+            src: '/icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/jpeg',
+            type: 'image/png',
             purpose: 'any maskable'
           }
         ]
