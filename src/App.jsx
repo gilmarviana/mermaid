cec157f1e9568988d699439fb10606b694e308f4
Gilmar Viana <70184391+gilmarviana@users.noreply.github.com>
2025-07-02 23:54:48 -0300

    pwa

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import PWAUpdatePrompt from './components/PWAUpdatePrompt'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <div style={{
-        margin: 0,
-        padding: 0,
-        overflow: 'hidden',
-        height: '100vh',
-        width: '100vw'
-      }}>
-        <iframe 
-          src="https://bibliaonline-90421.bubbleapps.io/" 
-          frameBorder="0" 
-          style={{
-            overflow: 'hidden',
-            height: '100%',
-            width: '100%',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0
-          }}
-          title="Evangelista Virtual"
-        />
-      </div>
-      <PWAUpdatePrompt />
-    </>
-  )
+  
+  
 }
 
 export default App
