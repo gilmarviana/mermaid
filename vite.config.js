bb22a3f01d4eb5bcf9dc6b37577a6cb0960203af
Gilmar Viana <70184391+gilmarviana@users.noreply.github.com>
2025-07-03 02:00:37 -0300

    Update icons to use real logo and fix Netlify build issues

--- vite.config.js
+++ vite.config.js
@@ -23,15 +23,15 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: 'https://c6f442293fa73eb40b2178fa58fd3742.cdn.bubble.io/cdn-cgi/image/w=192,h=192,f=auto,dpr=1,fit=contain/f1720976231275x392427182868092300/logo.jpg',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/jpeg',
             purpose: 'any maskable'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: 'https://c6f442293fa73eb40b2178fa58fd3742.cdn.bubble.io/cdn-cgi/image/w=512,h=512,f=auto,dpr=1,fit=contain/f1720976231275x392427182868092300/logo.jpg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/jpeg',
             purpose: 'any maskable'
           }
         ]
